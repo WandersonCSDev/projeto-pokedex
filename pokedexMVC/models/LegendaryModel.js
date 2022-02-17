89437aac613dd716f32ce6a19a510fdce8a9bf96
Wanderson C. Serra <wanderson.net@outlook.com.br>
2022-02-17 11:01:26 -0300

    17-02-2022

--- pokedexMVC/models/LegendaryModel.js
+++ pokedexMVC/models/LegendaryModel.js
@@ -1,8 +1,18 @@
-function LegendaryModel(id, name, description, type) {
-  this.id = id;
-  this.name = name;
-  this.description = description;
-  this.type = type;
+function LegendaryModel(id, name, description, type,
+    healthPoints, specialAttack, defense, attack, experience,
+    specialDefense) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.type = type;
+    this.healthPoints = healthPoints;
+    this.specialAttack = specialAttack;
+    this.defense = defense;
+    this.attack = attack;
+    this.experience = experience;
+    this.specialDefense = specialDefense;
 }
 
 module.exports = LegendaryModel;
+
+//TABLE - LEGENDARIES
--- pokedexMVC/service/LegendariesService.js
+++ pokedexMVC/service/LegendariesService.js
@@ -1,11 +1,21 @@
 const LegendaryModel = require('../models/LegendaryModel');
 
 const LegendariesService = {
-  listLegendaries: () => {
-    const pokemon1 = new LegendaryModel(1, 'Pokemon Zika','Morador da ZL', 'Maloca')
-    const pokemon2 = new LegendaryModel(1, 'Pokemon','Barra funda', 'Sem mundial')
-    return [pokemon1, pokemon2]
-  }
+    listLegendaries: () => {
+        const Mew = new LegendaryModel(
+            1,
+            'Mew',
+            'Mew é um Pokémon muito curioso e fofo que pode se transformar em qualquer outro tipo de Pokémon Pokémon. É um Pokémon de tipo psíquico.',
+            'Psychic',
+            '1000,000',
+            '1000,000',
+            '1000,000',
+            '1000,000',
+            '1000,000',
+            '1000,000',
+        )
+        return [Mew]
+    }
 }
 
 module.exports = LegendariesService;
