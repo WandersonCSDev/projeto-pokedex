657450ff468cf5e5b88756d48029a396058eb263
Wanderson C. Serra <wanderson.net@outlook.com.br>
2022-02-18 18:04:05 -0300

    18-02-2022

--- pokedexMVC/controllers/LegendariesController.js
+++ pokedexMVC/controllers/LegendariesController.js
@@ -1,10 +1,11 @@
 const LegendariesService = require('../service/LegendariesService');
 
 const controller = {
-  index: (request, response) => {
-    const legendariesList = LegendariesService.listLegendaries()
-    return response.json(legendariesList);
-  }
+    index: (request, response) => {
+        const legendariesList = LegendariesService.listLegendaries()
+        return response.json(legendariesList);
+    },
+
 }
 
 module.exports = controller;
--- pokedexMVC/routes/legendaries.js
+++ pokedexMVC/routes/legendaries.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controllers/LegendariesController');
 
-router.get('/', controller.index);
+/* GET home page. */
+router.get('/legend', function(req, res, next) {
+    res.render('legendaries');
+});
 
-
-module.exports = router
+module.exports = router;
