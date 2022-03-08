bf5f87bfd9b75b8a32b84547c66a70dfdbb9984f
Wanderson C. Serra <wanderson.net@outlook.com.br>
2022-03-08 01:14:36 -0300

    commits nivelamento

--- pokedexMVC/routes/index.js
+++ pokedexMVC/routes/index.js
@@ -3,14 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
-=======
     res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
->>>>>>> cdcf5ca43d92ed3d052466d7e29f9828e908c2e7
--- pokedexMVC/routes/legendaries.js
+++ pokedexMVC/routes/legendaries.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
 
 /* GET home page. */
 router.get('/legend', function(req, res, next) {
@@ -8,11 +7,3 @@ router.get('/legend', function(req, res, next) {
 });
 
 module.exports = router;
-=======
-const controller = require('../controllers/LegendariesController');
-
-router.get('/', controller.index);
-
-
-module.exports = router
->>>>>>> cdcf5ca43d92ed3d052466d7e29f9828e908c2e7
