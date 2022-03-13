5a9582930369d7787183fbe3fbb88b5a94adfe96
Wanderson C. Serra <wanderson.net@outlook.com.br>
2022-03-12 22:36:27 -0300

    master new

--- pokedexMVC/service/LegendariesService.js
+++ pokedexMVC/service/LegendariesService.js
@@ -1,7 +1,6 @@
 const LegendaryModel = require('../models/LegendaryModel');
 
 const LegendariesService = {
-<<<<<<< HEAD
     listLegendaries: () => {
         const Mew = new LegendaryModel(
             1,
@@ -17,13 +16,11 @@ const LegendariesService = {
         )
         return [Mew]
     }
-=======
   listLegendaries: () => {
     const pokemon1 = new LegendaryModel(1, 'Pokemon Zika','Morador da ZL', 'Maloca')
     const pokemon2 = new LegendaryModel(1, 'Pokemon','Barra funda', 'Sem mundial')
     return [pokemon1, pokemon2]
   }
->>>>>>> cdcf5ca43d92ed3d052466d7e29f9828e908c2e7
 }
 
 module.exports = LegendariesService;
