fc5d920a334eca6994677dc680b7302a92b3084f
Wanderson C. Serra <wanderson.net@outlook.com.br>
2022-03-12 22:34:07 -0300

    new mastre

--- pokedexMVC/controllers/LegendariesController.js
+++ pokedexMVC/controllers/LegendariesController.js
@@ -1,7 +1,6 @@
 const LegendariesService = require('../service/LegendariesService');
 
 const controller = {
-<<<<<<< HEAD
     index: (req, res) => {
         const { name } = req.query;
 
@@ -11,12 +10,10 @@ const controller = {
             legendary
         });
     }
-=======
   index: (request, response) => {
     const legendariesList = LegendariesService.listLegendaries()
     return response.json(legendariesList);
   }
->>>>>>> cdcf5ca43d92ed3d052466d7e29f9828e908c2e7
 }
 
 module.exports = controller;
