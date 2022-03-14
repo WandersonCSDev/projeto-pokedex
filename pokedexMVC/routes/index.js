ec4de8d9eae3a50098198a97fc1fce748fab3392
Wanderson C. Serra <wanderson.net@outlook.com.br>
2022-03-13 23:43:54 -0300

    review 22

--- pokedexMVC/routes/index.js
+++ pokedexMVC/routes/index.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index');
 });
+
+router.get('/legends', (req, res, next) => {
+    res.render('legendaries');
+})
 
 module.exports = router;
--- pokedexMVC/service/LegendariesService.js
+++ pokedexMVC/service/LegendariesService.js
@@ -1,21 +1,21 @@
 const LegendaryModel = require('../models/LegendaryModel');
 
 const LegendariesService = {
-  listLegendaries: () => {
-    const Mew = new LegendaryModel(
-      1,
-      'Mew',
-      'Its DNA is almost the same as Mews. However, its size and disposition are vastly different.',
-      'Psychic',
-      '1000,000',
-      '1000,000',
-      '1000,000',
-      '1000,000',
-      '1000,000',
-      '1000,000',
-    )
-    return [Mew]
-  }
+    listLegendaries: () => {
+        const Mew = new LegendaryModel(
+            1,
+            'Mew-Zika',
+            'Monstrinho desgraçado feio pra caramba, daquele desenho.',
+            'Psychic',
+            '1000,000',
+            '1000,000',
+            '1000,000',
+            '1000,000',
+            '1000,000',
+            '1000,000',
+        )
+        return [Mew]
+    }
 }
 
 module.exports = LegendariesService;
